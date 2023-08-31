a5ba46cddd4740b8501c258b3ae28d60779de2f8
Chan Pyae Phyo Aung <114207295+chanpyaephyoaung@users.noreply.github.com>
2023-08-31 23:14:20 +0630

    Delete and update files for chips list scrolling functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
 import gameRulesModalView from "./views/modal/gameRulesModalView";
-import chipsListContainerView from "./views/chips/chipsListContainerView";
 import chipsListView from "./views/chips/chipsListView";
 import { controlBetChipsList } from "./controllers/chipsControllers";
 
--- src/js/views/chips/chipsListView.js
+++ src/js/views/chips/chipsListView.js
@@ -1,14 +1,31 @@
 import View from "../View";
+import { CHIPS_LIST_CONTAINER_SCROLL_AMOUNT } from "../../config/chipsConfig";
 
 class ChipsListView extends View {
   _containerEl = document.querySelector(".chips__container");
+  _navigatorLeft = document.querySelector(".chips-container__navigator--left");
+  _navigatorRight = document.querySelector(".chips-container__navigator--right");
   _options = {
     clearBeforeAdding: false,
     position: "beforeend",
   };
 
+  constructor() {
+    super();
+    this._navigatorLeft.addEventListener("click", this._scrollLeft.bind(this));
+    this._navigatorRight.addEventListener("click", this._scrollRight.bind(this));
+  }
+
   addHandlerRender(handler) {
     window.addEventListener("load", handler);
+  }
+
+  _scrollLeft() {
+    this._containerEl.scrollLeft -= CHIPS_LIST_CONTAINER_SCROLL_AMOUNT;
+  }
+
+  _scrollRight() {
+    this._containerEl.scrollLeft += CHIPS_LIST_CONTAINER_SCROLL_AMOUNT;
   }
 
   _generateMarkup() {
